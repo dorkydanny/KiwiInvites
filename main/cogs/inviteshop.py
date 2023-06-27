22da47a095795d1753690467bdf517fb7177fde9
Daniel Borro <dborro10@gmail.com>
2023-06-27 13:51:38 +1200

    buying system complete

--- main/cogs/inviteshop.py
+++ main/cogs/inviteshop.py
@@ -1,11 +1,14 @@
 import discord  
 from discord.ext import commands
 from discord import app_commands
+import json
 
 class invitesshop(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        f = open("main/invites.json")
+        self.data = json.load(f)
     
     @app_commands.command(name="inviteshop", description="Buy perks for invites")
     async def inviteshop(self, interaction: discord.Interaction):
@@ -16,6 +19,26 @@ class invitesshop(commands.Cog):
         embed.add_field(name="One Month Kiwi", value="30 Invites", inline=1)
         embed.set_footer(text="Property of Kiwi.")
         await interaction.response.send_message(embed=embed)
+    
+    @app_commands.command(name="buy", description="Buy a rank for invites.")
+    @app_commands.describe(ranks="Ranks to choose from")
+    
+    @app_commands.choices(ranks=[ 
+        discord.app_commands.Choice(name="Premium", value="premium"),
+        discord.app_commands.Choice(name="Kiwi", value="kiwi")
+    ])
+    async def buy(self, interaction: discord.Interaction, ranks: discord.app_commands.Choice[str]):
+        ranks = ranks.value
+        prices = {"premium" : 15, "kiwi" : 30}
+        if self.data[str(interaction.user.id)] >= prices[ranks]:
+            self.data[str(interaction.user.id)] -= prices[ranks]
+            with open("main/invites.json", "w") as outfile:
+                json.dump(self.data, outfile)
+            await interaction.response.send_message(f"Succesfuly brought {ranks} for {prices[ranks]} invites.")
+        await interaction.response.send_message("You do not have enough invites.\n" +
+                                                f"You have: {self.data[str(interaction.user.id)]}\n" +
+                                                f"You need: {prices[ranks]}")
+    
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(invitesshop(bot))
